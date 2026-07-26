d5cdda2e73dbe6be38bd15a462e5853ec01d8bdd
AIJUNIORS <agenticanalysis@gmail.com>
2026-07-26 17:05:39 +0000

    Add backend package initialization and sources module

---
+++ backend/sources.py
@@ -0,0 +1,11 @@
+"""
+Supported Data Sources
+"""
+
+SOURCES = {
+    "NSE": "National Stock Exchange",
+    "Yahoo": "Yahoo Finance",
+    "MCX": "Multi Commodity Exchange",
+    "RBI": "Reserve Bank of India",
+    "MOSPI": "Ministry of Statistics"
+}
